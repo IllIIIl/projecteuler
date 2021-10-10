a380ebbd1c2199bd13c68ee6a5b278142b6995e7
Zac Childress (oz_) <91855780+OZsoft-zac@users.noreply.github.com>
2021-10-10 16:10:34 -0500

    Add files via upload

--- euler003.cpp
+++ euler003.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 #include <math.h>
-
 using namespace std;
-
-//just print out the factors and the biggest one is the answer
-void primeFactors(long n) {
-   while (n % 2 == 0){
-      cout << "2\t";
-      n = n / 2;
-   }
-   for (int i = 3; i <= sqrt(n); i = i + 2){
-      while (n % i == 0){
-         cout << i << "\t";
-         n = n / i;
-      }
-   }
-   if (n > 2)
-   cout << n << "\t";
-}
 int main() {
-   long n = 600851475143;
-   cout << "Prime factors of " << n << " are :\t";
-   primeFactors(n);
-    
+   long x = 600851475143;
+    while (x % 2 == 0){
+       cout << "2\t";
+       x = x / 2;
+    }
+    for (int i = 3; i <= sqrt(x); i = i + 2){
+       while (x % i == 0){
+          cout << i << "\t";
+          x = x / i;
+       }
+    }
+    if (x > 2)
+        cout << x << "\t";
    return 0;
 }
